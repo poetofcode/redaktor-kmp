a3a9aa047402e37186732eb7a7db4ab6f703ea76
poetofcode <cyber-post@yandex.ru>
2025-12-14 01:47:03 +0300

    Android ready

--- client/composeApp/src/commonMain/kotlin/presentation/screens/catalogScreen/CatalogScreen.kt
+++ client/composeApp/src/commonMain/kotlin/presentation/screens/catalogScreen/CatalogScreen.kt
@@ -3,11 +3,24 @@ package presentation.screens.catalogScreen
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.*
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Cancel
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Done
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Link
 import androidx.compose.material3.Button
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
@@ -43,7 +56,7 @@ class CatalogScreen(
     override fun Content() {
         BackHandler {
             offerIntent(CatalogIntent.OnCancelEditClick)
-            true
+            state.isEditing
         }
         Column {
             Toolbar()
