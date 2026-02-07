32eb7875db1dc6a5ce743d8a66f08385bfc4ab1a
poetofcode <cyber-post@yandex.ru>
2026-02-07 23:20:00 +0300

    Fix

--- client/composeApp/src/androidMain/kotlin/specific/AndroidContentProvider.kt
+++ client/composeApp/src/androidMain/kotlin/specific/AndroidContentProvider.kt
@@ -4,24 +4,25 @@ import android.content.Context
 import data.utils.ContentProvider
 import java.io.File
 
-class AndroidContentProvider constructor(
-    val context: Context,
-    val fileName: String,
+class AndroidContentProvider(
+    private val context: Context,
+    private val fileName: String,
 ) : ContentProvider {
 
     override fun provideContent(): String {
-        val cachePath = File(context.cacheDir, "appcache")
+        val cachePath = File(context.filesDir, "appdata")
         cachePath.mkdirs()
         val stream = File("$cachePath/$fileName").bufferedReader()
         return stream.use { it.readText() }
     }
 
     override fun saveContent(content: String) {
-        val cachePath = File(context.cacheDir, "appcache")
+        val cachePath = File(context.filesDir, "appdata")
         cachePath.mkdirs()
         val stream = File("$cachePath/$fileName")
         stream.printWriter().use {
             it.write(content)
+            it.flush()
         }
     }
 
